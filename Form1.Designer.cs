668a00c72f3def61dd4f9072603753809a1693af
Kristijan Čulina <75842348+qbitdone@users.noreply.github.com>
2021-08-07 23:37:35 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace UFCfinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.zemljatxt = new System.Windows.Forms.TextBox();
+            this.prezimetxt = new System.Windows.Forms.TextBox();
+            this.imetxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.u_imetxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.u_prezimetxt = new System.Windows.Forms.TextBox();
+            this.u_idtxt = new System.Windows.Forms.TextBox();
+            this.u_zemljatxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.zemljatxt);
+            this.groupBox1.Controls.Add(this.prezimetxt);
+            this.groupBox1.Controls.Add(this.imetxt);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(18, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(297, 204);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CREATE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zemljatxt
+            // 
+            this.zemljatxt.Location = new System.Drawing.Point(109, 141);
+            this.zemljatxt.Name = "zemljatxt";
+            this.zemljatxt.Size = new System.Drawing.Size(100, 20);
+            this.zemljatxt.TabIndex = 7;
+            // 
+            // prezimetxt
+            // 
+            this.prezimetxt.Location = new System.Drawing.Point(109, 105);
+            this.prezimetxt.Name = "prezimetxt";
+            this.prezimetxt.Size = new System.Drawing.Size(100, 20);
+            this.prezimetxt.TabIndex = 6;
+            // 
+            // imetxt
+            // 
+            this.imetxt.Location = new System.Drawing.Point(109, 67);
+            this.imetxt.Name = "imetxt";
+            this.imetxt.Size = new System.Drawing.Size(100, 20);
+            this.imetxt.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(16, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ZEMLJA: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(16, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "IME: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(16, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PREZIME: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox2.Location = new System.Drawing.Point(18, 231);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(648, 181);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "READ / VIEW DATA";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(636, 149);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(15, 146);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 19);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "ZEMLJA: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(15, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 19);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "IME: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 19);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "PREZIME: ";
+            // 
+            // u_imetxt
+            // 
+            this.u_imetxt.Location = new System.Drawing.Point(108, 67);
+            this.u_imetxt.Name = "u_imetxt";
+            this.u_imetxt.Size = new System.Drawing.Size(100, 20);
+            this.u_imetxt.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(15, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "ID: ";
+            // 
+            // u_prezimetxt
+            // 
+            this.u_prezimetxt.Location = new System.Drawing.Point(108, 105);
+            this.u_prezimetxt.Name = "u_prezimetxt";
+            this.u_prezimetxt.Size = new System.Drawing.Size(100, 20);
+            this.u_prezimetxt.TabIndex = 6;
+            // 
+            // u_idtxt
+            // 
+            this.u_idtxt.Location = new System.Drawing.Point(108, 33);
+            this.u_idtxt.Name = "u_idtxt";
+            this.u_idtxt.Size = new System.Drawing.Size(100, 20);
+            this.u_idtxt.TabIndex = 4;
+            // 
+            // u_zemljatxt
+            // 
+            this.u_zemljatxt.Location = new System.Drawing.Point(108, 143);
+            this.u_zemljatxt.Name = "u_zemljatxt";
+            this.u_zemljatxt.Size = new System.Drawing.Size(100, 20);
+            this.u_zemljatxt.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.u_zemljatxt);
+            this.groupBox3.Controls.Add(this.u_idtxt);
+            this.groupBox3.Controls.Add(this.u_prezimetxt);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.u_imetxt);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox3.Location = new System.Drawing.Point(368, 9);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(292, 204);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "UPDATE / DELETE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UFCfinal.Properties.Resources.wp2511548__1_;
+            this.ClientSize = new System.Drawing.Size(685, 424);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "UFC";
+            this.TransparencyKey = System.Drawing.Color.DarkGray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox zemljatxt;
+        private System.Windows.Forms.TextBox prezimetxt;
+        private System.Windows.Forms.TextBox imetxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox u_imetxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox u_prezimetxt;
+        private System.Windows.Forms.TextBox u_idtxt;
+        private System.Windows.Forms.TextBox u_zemljatxt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UFCfinal.myclass;
+using MySql.Data.MySqlClient;
+
+namespace UFCfinal
+{
+    public partial class Form1 : Form
+    {
+        CRUD crud = new CRUD();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Read();
+            UClear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // CREATE
+            Create();
+            Read();
+            Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // UPDATE
+            Update();
+            Read();
+            UClear();
+      
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // DELETE
+            Delete();
+            Read();
+            UClear();
+        }
+
+        // READ
+        public void Read()
+        {
+            dataGridView1.DataSource = null;
+            crud.ReadData();
+            dataGridView1.DataSource = crud.dt;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[0].HeaderText = "ID";
+            dataGridView1.Columns[1].HeaderText = "IME";
+            dataGridView1.Columns[2].HeaderText = "PREZIME";
+            dataGridView1.Columns[3].HeaderText = "ZEMLJA";
+
+        }
+
+        // CREATE
+        public void Create()
+        {
+            crud.ime_sudac = imetxt.Text;
+            crud.prezime_sudac = prezimetxt.Text;
+            crud.zemlja_id = int.Parse(zemljatxt.Text);
+            crud.CreateData();
+        }
+
+        // UPDATE
+        public void Update()
+        {
+            crud.ime_sudac = u_imetxt.Text;
+            crud.prezime_sudac = u_prezimetxt.Text;
+            crud.zemlja_id = int.Parse(u_zemljatxt.Text);
+            crud.id_sudac = int.Parse(u_idtxt.Text);
+            crud.UpdateData();
+        }
+
+        // DELETE
+        public void Delete()
+        {
+            crud.id_sudac = int.Parse(u_idtxt.Text);
+            crud.DeleteData();
+
+        }
+
+        // CLEAR
+        public void Clear()
+        {
+            imetxt.Text = "";
+            prezimetxt.Text = "";
+            zemljatxt.Text = "";
+        }
+
+        // UPDATE CLEAR
+        public void UClear()
+        {
+            u_idtxt.Text = "";
+            u_imetxt.Text = "";
+            u_prezimetxt.Text = "";
+            u_zemljatxt.Text = "";
+        }
+
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            // GET DATA
+            DataGridView senderGrid = (DataGridView)sender;
+
+            try
+            {
+                if (dataGridView1.Rows[senderGrid.CurrentCell.RowIndex].Cells[senderGrid.CurrentCell.ColumnIndex].Value != null)
+                {
+                    u_idtxt.Text = (dataGridView1.Rows[senderGrid.CurrentCell.RowIndex].Cells[0].Value.ToString());
+                    u_imetxt.Text = (dataGridView1.Rows[senderGrid.CurrentCell.RowIndex].Cells[1].Value.ToString());
+                    u_prezimetxt.Text = (dataGridView1.Rows[senderGrid.CurrentCell.RowIndex].Cells[2].Value.ToString());
+                    u_zemljatxt.Text = (dataGridView1.Rows[senderGrid.CurrentCell.RowIndex].Cells[3].Value.ToString());
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ne klikaj atribute!");
+            }
+        }
+
+    }
+}
